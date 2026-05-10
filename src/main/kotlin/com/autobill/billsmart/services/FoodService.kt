408fdc72225ca55a71a5f41519fc0da7449fa438
dilip <dilip.mobiledev@gmail.com>
2026-05-10 09:17:43 +0530

    added food update api

--- src/main/kotlin/com/autobill/billsmart/services/FoodService.kt
+++ src/main/kotlin/com/autobill/billsmart/services/FoodService.kt
@@ -5,6 +5,7 @@ import com.autobill.billsmart.dto.FoodResponse
 
 interface FoodService {
     fun createFood(restroId: Long, req: FoodRequest): FoodResponse
+    fun updateFood(id: Long, req: FoodRequest): FoodResponse
     fun getAllFoods(restroId: Long): List<FoodResponse>
     fun getFood(id: Long): FoodResponse?
     fun deleteFood(id: Long)
